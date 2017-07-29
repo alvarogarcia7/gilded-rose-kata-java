4576f1e7992f077f1dc568bee44dfa5f4d5dd93f
Alvaro Garcia <derecerca@gmail.com>
2017-07-29 16:10:44 +0200

    Add a longer period for the case 1

---
+++ src/test/golden-master/case-02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail #do not enable 'f'. Actually interested in file glob expansion
+
+case "$1" in 
+    record)
+        java -jar target/*.jar 10 < src/test/resources/golden-master/2.input > src/test/resources/golden-master/2.expected
+        ;;
+    play)
+        java -jar target/*.jar < src/test/resources/golden-master/2.input > src/test/resources/golden-master/2.actual
+        diff src/test/resources/golden-master/2.actual src/test/resources/golden-master/2.expected
+        ;;
+esac
+

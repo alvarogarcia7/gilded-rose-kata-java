7bc0a773a4af4706f3479f3f644b2d41b61d5eaf
Alvaro Garcia <derecerca@gmail.com>
2017-07-29 16:25:51 +0200

    Refactor: extract explaining method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (isAgedBrie(items[i])
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !isABackstagePass(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -20,7 +20,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isABackstagePass(items[i])) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -42,7 +42,7 @@ class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (isAgedBrie(items[i])) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isABackstagePass(items[i])) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
@@ -58,6 +58,10 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isABackstagePass (final Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
     private boolean isAgedBrie (final Item item) {

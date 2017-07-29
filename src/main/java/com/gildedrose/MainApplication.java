6bdf9f0037cc82f098cc6c139fce1a7f6b5f27e9
Alvaro Garcia <derecerca@gmail.com>
2017-07-29 12:15:08 +0200

    Defect: missing the main class

--- src/main/java/com/gildedrose/MainApplication.java
+++ src/main/java/com/gildedrose/MainApplication.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");

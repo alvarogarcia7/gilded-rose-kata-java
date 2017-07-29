a0eab4ff4c09cd39210a02bdf9e63e542338ffce
Alvaro Garcia <derecerca@gmail.com>
2017-07-29 12:17:26 +0200

    Refactor: extract method

--- src/main/java/com/gildedrose/MainApplication.java
+++ src/main/java/com/gildedrose/MainApplication.java
@@ -7,17 +7,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+        Item[] items = readItems();
 
 
         int days = 2;
@@ -26,6 +16,20 @@ public class MainApplication {
         }
 
         updateQuality(items, days);
+    }
+
+    private static Item[] readItems () {
+        return new Item[] {
+                    new Item("+5 Dexterity Vest", 10, 20), //
+                    new Item("Aged Brie", 2, 0), //
+                    new Item("Elixir of the Mongoose", 5, 7), //
+                    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                    // this conjured item does not work properly yet
+                    new Item("Conjured Mana Cake", 3, 6) };
     }
 
     private static void updateQuality (final Item[] items, final int days) {
